1b6c7e8b0cea3f03e4a24fcf2bf512d0fc34ea58
Cédric Françoys <cedricfrancoys@gmail.com>
2025-01-25 20:50:44 +0100

    refactored to handle only sengding of alerts

--- inventory/actions/server/send-alerts.php
+++ inventory/actions/server/send-alerts.php
@@ -7,13 +7,12 @@
 
 use core\Mail;
 use equal\email\Email;
-use inventory\server\AlertPolicy;
-use inventory\server\AlertTrigger;
+use inventory\server\Alert;
 use inventory\server\Server;
-use inventory\server\Status;
+use inventory\server\Instance;
 
 [$params, $providers] = eQual::announce([
-    'description'       => "Sends servers/instances alerts if their triggers match the last servers status(es).",
+    'description'       => "Sends servers/instances pending alerts to users (email).",
     'params'            => [],
     'response'          => [
         'content-type'      => 'application/json',
@@ -28,64 +27,6 @@ use inventory\server\Status;
  */
 ['context' => $context] = $providers;
 
-
-/**
- * Methods
- */
-
-/**
- * Comparison methods to check the server status' value against the trigger's value
- */
-$comparison_methods = [
-    'eq'                => function($value, $trigger_value) { return $value === $trigger_value; },
-    'ne'                => function($value, $trigger_value) { return $value !== $trigger_value; },
-    'gt'                => function($value, $trigger_value) { return $value > $trigger_value; },
-    'gte'               => function($value, $trigger_value) { return $value >= $trigger_value; },
-    'lt'                => function($value, $trigger_value) { return $value < $trigger_value; },
-    'lte'               => function($value, $trigger_value) { return $value <= $trigger_value; },
-    'contains'          => function($value, $trigger_value) { return strpos($value, $trigger_value) !== false; },
-    'does_not_contain'  => function($value, $trigger_value) { return strpos($value, $trigger_value) === false; }
-];
-
-/**
- * Returns true if the given alert must be sent bases on server statuses and alert triggers
- *
- * @param array $alert
- * @param array $statuses Server or instance statuses
- * @return bool
- */
-$mustSendAlert = function(array $alert, array $statuses) use($comparison_methods) {
-    $must_trigger = count($alert['alert_triggers_ids']) > 0;
-    foreach($alert['alert_triggers_ids'] as $trigger) {
-        if($trigger['repetition'] > count($statuses)) {
-            $must_trigger = false;
-            break;
-        }
-
-        for($i = 0; $i < $trigger['repetition']; $i++) {
-            $status = $statuses[$i];
-
-            $status_value = AlertTrigger::getServerStatusValue($trigger['key'], $status);
-            if(is_null($status_value)) {
-                $must_trigger = false;
-                break 2;
-            }
-
-            $status_value = AlertTrigger::getAdaptedValue($trigger['key'], $status_value);
-            $trigger_value = AlertTrigger::getAdaptedValue($trigger['key'], $trigger['value']);
-
-            if(
-                !in_array($trigger['operator'], array_keys($comparison_methods))
-                || !$comparison_methods[$trigger['operator']]($status_value, $trigger_value)
-            ) {
-                $must_trigger = false;
-                break 2;
-            }
-        }
-    }
-
-    return $must_trigger;
-};
 
 /**
  * Sends the given alert to all users link for given server
@@ -125,10 +66,6 @@ $sendAlert = function(array $alert, string $alert_email_subject) {
 };
 
 
-/**
- * Action
- */
-
 $servers = Server::search([
         ['server_type', 'in', ['b2', 'k2', 's2', 'admin']],
         ['send_alerts', '=', true]
@@ -140,137 +77,45 @@ $servers = Server::search([
     ])
     ->get();
 
-$alerts = AlertPolicy::search()
+$alerts = Alert::search(['status', '=', 'pending'])
     ->read([
+        'id',
         'name',
-        'alert_type',
-        'users_ids'             => ['login'],
-        'groups_ids'            => ['users_ids' => ['login']],
-        'alert_triggers_ids'    => ['name', 'key', 'operator', 'value', 'repetition']
+        'server_id' => ['id', 'name'],
+        'instance_id' => ['id', 'name'],
+        'alert_policy_id' => [
+            'alert_type',
+            'users_ids'             => ['login'],
+            'groups_ids'            => ['users_ids' => ['login']],
+            'alert_triggers_ids'    => ['name', 'key', 'operator', 'value', 'repetition']
+        ]
     ])
     ->get();
 
-// Send servers alerts if triggers matches
-foreach($servers as $server) {
-    $server_alerts = array_filter(
-        $alerts,
-        function ($alert) use($server) {
-            return in_array($alert['alert_type'], ['all', $server['server_type']]);
-        }
-    );
-
-    if(count($server_alerts) === 0) {
-        // No alert configured
-        continue;
-    }
-
-    // Handle trigger repetition to get right quantity of statuses to check against triggers
-    $max_repetition = 1;
-    foreach($server_alerts as $alert) {
-        foreach($alert['alert_triggers_ids'] as $alert_trigger) {
-            $max_repetition = max($max_repetition, $alert_trigger['repetition']);
-        }
-    }
-
-    // Get the quantity of server statuses needed to check all the alerts
-    $server_statuses = Status::search(
-            ['server_id', '=', $server['id']],
-            [
-                'sort'  => ['created' => 'desc'],
-                'limit' => $max_repetition
-            ]
-        )
-        ->read(['up', 'status_data'])
-        ->get();
-
-    if(empty($server_statuses)) {
-        // No statuses yet
-        continue;
-    }
-
-    $server_statuses_data = array_map(
-        function ($status_data) {
-            return json_decode($status_data, true);
-        },
-        array_column($server_statuses, 'status_data')
-    );
-
-    // Send alerts if triggers matches
-    foreach($server_alerts as $alert) {
-        if(!$mustSendAlert($alert, $server_statuses_data)) {
-            continue;
-        }
-
-        $sendAlert(
-            $alert,
-            "Alert \"{$alert['name']}\" for server {$server['name']}"
-        );
-    }
-}
-
-// Send instances alerts if triggers matches
-foreach($servers as $server) {
-    foreach($server['instances_ids'] as $instance) {
-        if(!$instance['send_alerts']) {
-            continue;
-        }
-
-        $instance_alerts = array_filter(
-            $alerts,
-            function ($alert) {
-                return in_array($alert['alert_type'], ['all', 'b2_instance']);
-            }
-        );
-
-        if(count($instance_alerts) === 0) {
-            // No alert configured
-            continue;
-        }
-
-        // Handle trigger repetition to get right quantity of statuses to check against triggers
-        $max_repetition = 1;
-        foreach($instance_alerts as $alert) {
-            foreach($alert['alert_triggers_ids'] as $alert_trigger) {
-                $max_repetition = max($max_repetition, $alert_trigger['repetition']);
-            }
-        }
-
-        // Get the quantity of instance statuses needed to check all the alerts
-        $instance_statuses = Status::search(
-                ['instance_id', '=', $instance['id']],
-                [
-                    'sort'  => ['created' => 'desc'],
-                    'limit' => $max_repetition
-                ]
-            )
-            ->read(['up', 'status_data'])
-            ->get();
-
-        if(empty($instance_statuses)) {
-            // No statuses yet
-            continue;
-        }
-
-        $instance_statuses_data = array_map(
-            function ($status_data) {
-                return json_decode($status_data, true);
-            },
-            array_column($instance_statuses, 'status_data')
-        );
-
-        // Send alerts if triggers matches
-        foreach($instance_alerts as $alert) {
-            if(!$mustSendAlert($alert, $instance_statuses_data)) {
-                continue;
-            }
-
+foreach($alerts as $id => $alert) {
+    if($alert['server_id']) {
+        $server = Server::id($alert['server_id']['id'])->read(['send_alerts'])->first();
+        if($server['send_alerts']) {
             $sendAlert(
-                $alert,
-                "Alert \"{$alert['name']}\" for instance {$instance['name']} (server {$server['name']})"
+                $alert['alert_policy_id'],
+                "Alert \"{$alert['name']}\" for server {$alert['server_id']['name']}"
+            );
+        }
+
+    }
+    else if($alert['instance_id']) {
+        $instance = Instance::id($alert['instance_id']['id'])->read(['send_alerts'])->first();
+
+        if($instance['send_alerts']) {
+            $sendAlert(
+                $alert['alert_policy_id'],
+                "Alert \"{$alert['name']}\" for instance {$alert['instance_id']['name']}"
             );
         }
     }
+    Alert::id($id)->update(['status' => 'sent']);
 }
+
 
 $context->httpResponse()
         ->status(204)
